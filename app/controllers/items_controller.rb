68e867b44a0eade1b2d49482488267101bbf21cc
hidetsugu-o <osyame@gmail.com>
2020-12-14 14:14:21 +0900

    アクションとルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :profile, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
