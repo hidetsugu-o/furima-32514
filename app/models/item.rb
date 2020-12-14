c75558fcab52296ff250e62ffe3c59e7bc5b00b5
hidetsugu-o <osyame@gmail.com>
2020-12-14 12:45:06 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+
+  validates :title, :profile, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, :image, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
   belongs_to :user
   has_one_attached :image
 end
