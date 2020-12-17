8380993c1d5269bf77f8ebfcc72c43585cde5378
hidetsugu-o <osyame@gmail.com>
2020-12-17 14:39:25 +0900

    Formオブジェクトを実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,8 +1,23 @@
 class BuyersController < ApplicationController
   def index
+    @buyer_shipping_address = BuyerShippingAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
+    @buyer_shipping_address = BuyerShippingAddress.new(buyer_params)
+    if @buyer_shipping_address.valid?
+      @buyer_shipping_address.save
+      redirect_to root_path
+    else
+      @buyer_shipping_address = BuyerShippingAddress.new(buyer_params[:errors])
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+  def buyer_params
+    params.require(:buyer_shipping_address).permit(:user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :buyer_id)
   end
 end
---
+++ app/models/buyer_shipping_address.rb
@@ -0,0 +1,21 @@
+class BuyerShippingAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :buyer_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :municipality
+    validates :address
+    validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
+  end
+
+  def save
+    buyer = Buyer.create(user_id: @item.user_id, item_id: @item.id)
+    ShippingAddress.create(
+                            postal_code: postal_code, prefecuture_id: prefecture_id, 
+                            municipality: municipality, address: address, building: building, 
+                            phone_number: phone_number, buyer_id: buyer.id
+                          )
+  end
+end
