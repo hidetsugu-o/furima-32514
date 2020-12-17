95d89490f51b6e848966f5934b8fdd08d9f29de6
hidetsugu-o <osyame@gmail.com>
2020-12-17 11:26:50 +0900

    ShippingAddressモデルにバリデーションを記述

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,11 @@
 class ShippingAddress < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id
+    validates :municipality
+    validates :address
+    validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
+  end
+
   belongs_to :buyer
 end
