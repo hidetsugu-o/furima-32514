7657f9cb117f3b946b0f44091abe96f0520b575c
hidetsugu-o <osyame@gmail.com>
2020-12-17 14:39:03 +0900

    アクティブハッシュのアソシエーション設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,11 +1,6 @@
 class ShippingAddress < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id
-    validates :municipality
-    validates :address
-    validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
-  end
-
   belongs_to :buyer
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
