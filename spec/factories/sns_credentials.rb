5113ed3f561027678b99bca18001e19d57afda38
hidetsugu-o <osyame@gmail.com>
2020-12-25 12:09:55 +0900

    SnsCredentialモデルの単体テストコード記述

--- spec/factories/sns_credentials.rb
+++ spec/factories/sns_credentials.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :sns_credential do
+    provider { "facebook" }
+    uid { 0000000000000000 }
     
+    association :user
   end
 end
--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SnsCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @sns_credential = FactoryBot.build(:sns_credential)
+  end
+
+  context '新規登録がうまく行く時' do
+    it 'userの登録がなくても登録できる' do
+      @sns_credential.user = nil
+      expect(@sns_credential).to be_valid
+    end
+  end
+  
 end
